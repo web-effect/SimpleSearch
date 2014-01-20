ab836e5844abc0fc69a7365023d00980c6d47932
JP DeVries <johnpaul.devries@gmail.com>
2014-01-19 21:48:03 -0800

    Setting pkg release to beta1
    I don’t like building to within the repo :p
    going to tweet a download link

--- _build/build.transport.php
+++ _build/build.transport.php
@@ -40,7 +40,7 @@ set_time_limit(0);
 define('PKG_NAME','SimpleSearch');
 define('PKG_NAME_LOWER','simplesearch');
 define('PKG_VERSION','1.8.1');
-define('PKG_RELEASE','pl');
+define('PKG_RELEASE','beta1');
 
 /* define sources */
 $root = dirname(dirname(__FILE__)).'/';
